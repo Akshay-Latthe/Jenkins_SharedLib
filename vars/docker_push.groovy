760b68c1998105abebdf8f345bbc9d8b66e2312a
Akshay Latthe <137057440+Akshay-Latthe@users.noreply.github.com>
2025-05-24 14:35:49 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+def call(String Project, String ImageTag, String dockerhubuser, String credentialsId ){
+  withCredentials([usernamePassword(credentialsId: ${credentialsId}, passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
